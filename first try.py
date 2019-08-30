8702936d3f12b189372381a3563721f51379d300
OPminionwave <jellebennik@gmail.com>
2019-08-30 16:51:47 +0200

    fixed a self reading issue so it would react to itself

--- first try.py
+++ first try.py
@@ -14,16 +14,17 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith("|"):
+    if message.content.startswith("|") and message.author.id != 616697441965572116:
      if message.author.id == 525876114409783336:
          await message.channel.send("mladjo, u know it wont work...")
-     elif message.author.id != 199951806896406528:
+     elif message.author.id != 199951806896406528 :
          await message.channel.send("this bot is for my master only")
      elif message.author.id == 199951806896406528:
-        if message.content.startswith("|r"):
+        if message.content.startswith("|r") :
             await message.channel.send(message.content[3:])
-        elif message.content.startswith("|s") and message.author != bot.user:
+        elif message.content.startswith("|s"):
             await message.channel.send("fixed <:morningsol:473551130379550742>")
+
 
 
 bot.run(open("token.txt").read())
